147fde68f03de5469e7f79076becbdc3ebfaa1e4
Lucile Jones <jones.lucile@gmail.com>
2024-08-13 14:01:49 -0600

    added frontend client

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+
+  return (
+    <>
+      <p>App works!</p>
+    </>
+  )
+}
+
+export default App
