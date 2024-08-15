042cc5c21069ae5e820e4426cbe56e65d69ac3b1
Lucile Jones <jones.lucile@gmail.com>
2024-08-15 10:18:16 -0600

    initial setup for user context

---
+++ client/src/context/UserProvider.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export const UserContext = React.createContext();
+
+export default function UserProvider(props) {
+    const initState = {
+        user: {},
+        token: "",
+        students: []
+    }
+
+    const [userState, setUserState] = useState(initState);
+
+    return (
+        <UserContext.Provider value={{
+            ...userState
+        }}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
